2f00f37dd663157910c8d18f493e742479c5ec22
Brandon Nelson <bksnelson1@gmail.com>
2024-03-25 21:45:28 -1000

    contacts collection finished in 12 minutes

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -3,17 +3,17 @@ import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
-import Contact from '../components/Contact';
 import { Contacts } from '../../api/contact/Contacts';
+import ContactAdmin from '../components/ContactAdmin';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListContacts = () => {
+const ListContactsAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe(Contacts.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -28,10 +28,10 @@ const ListContacts = () => {
       <Row className="justify-content-center">
         <Col>
           <Col className="text-center">
-            <h2>List Contacts</h2>
+            <h2>List Contacts (Admin)</h2>
           </Col>
           <Row xs={1} md={2} lg={3}>
-            {contacts.map((contact) => (<Col key={contact._id}><Contact contact={contact} /></Col>))}
+            {contacts.map((contact) => (<Col key={contact._id}><ContactAdmin contact={contact} /></Col>))}
           </Row>
         </Col>
       </Row>
@@ -39,4 +39,4 @@ const ListContacts = () => {
   ) : <LoadingSpinner />);
 };
 
-export default ListContacts;
+export default ListContactsAdmin;
