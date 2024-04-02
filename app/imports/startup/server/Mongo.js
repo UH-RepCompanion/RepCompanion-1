1d23c37bfbfdfa4274f825bd19659e7cb83f5745
Brandon Nelson <bksnelson1@gmail.com>
2024-04-02 09:24:53 -1000

    cleanedup

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,28 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Contacts } from '../../api/contact/Contacts';
 
-/* eslint-disable no-console */
-
-// Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 const addContacts = (contact) => {
   console.log(`  Adding: ${contact.lastName} (${contact.owner})`);
   Contacts.collection.insert(contact);
 };
 
-// Initialize the StuffsCollection if empty.
+// Initialize the ContactsCollection if empty.
 if (Contacts.collection.find().count() === 0) {
   if (Meteor.settings.defaultContacts) {
     console.log('Creating default contacts.');
