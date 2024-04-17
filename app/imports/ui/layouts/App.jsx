db7e574de2549aedb7b27e77c5cd134289cd6977
Anthony A. Tirabassi (T) <143787394+t-tirabassi@users.noreply.github.com>
2024-04-16 19:16:43 -1000

    pt 1

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -18,6 +18,7 @@ import Profiles from '../pages/Profiles';
 import UserProfile from '../pages/UserProfile';
 import About from '../pages/About';
 import Filter from '../pages/Filter';
+import Policy from '../pages/Policy';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => {
@@ -39,6 +40,7 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signout" element={<SignOut />} />
           <Route path="/about" element={<About />} />
+          <Route path="/policy" element={<Policy />} />
           <Route path="/editprofile" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
           <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
           <Route path="/filter" element={<ProtectedRoute><Filter /></ProtectedRoute>} />m
