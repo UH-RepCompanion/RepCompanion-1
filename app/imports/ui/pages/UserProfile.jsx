b2740d8f886af37be765d17a83c0d58867297410
Anthony A. Tirabassi (T) <143787394+t-tirabassi@users.noreply.github.com>
2024-04-16 19:55:05 -1000

    edit profile btn

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Card, Row, Image } from 'react-bootstrap';
+import { Container, Col, Card, Row, Image, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
@@ -42,7 +42,7 @@ const UserProfile = () => {
   }, []);
 
   return ready ? (
-    <Container id={PageIDs.homePage} className="d-flex justify-content-center align-items-center" style={pageStyle}>
+    <Container id={PageIDs.homePage} className="d-flex justify-content-center align-items-center infofooter" style={pageStyle}>
       <Row className="justify-content-center align-items-center">
         <Col>
           <Col className="justify-content-center text-center"><h2 style={{ color: 'white' }}>{profile?.firstName}&apos;s Profile</h2></Col>
@@ -61,7 +61,7 @@ const UserProfile = () => {
               <Row>
                 <Col xs={6}><Card.Text style={{ marginBottom: '20px' }}><strong>Interests:</strong> {interests.join(', ')}</Card.Text></Col>
                 <Col xs={6}><Card.Text style={{ marginBottom: '20px', color: 'black' }}><strong>Tags:</strong> {tags.join(', ')}</Card.Text></Col>
-                <Link to="/editprofile" style={{ position: 'absolute', bottom: '10px', left: '500px' }}>Edit Profile</Link>
+                <Button className="text-start" variant="dark" id="landing-page-button" style={{ position: 'absolute', bottom: '10px', left: '465px', color: 'white', width: '120px' }} as={Link} to="/editprofile">Edit Profile</Button>
               </Row>
             </Card.Body>
           </Card>
