c2362117e54fb9c6f715b20456a790fd42bad665
Anthony A. Tirabassi (T) <143787394+t-tirabassi@users.noreply.github.com>
2024-04-27 17:37:30 -1000

    info footer w/ pillars (solid, transparent option available)

--- app/imports/ui/components/SecondSection.jsx
+++ app/imports/ui/components/SecondSection.jsx
@@ -1,47 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Image, Row } from 'react-bootstrap';
 
-const SecondSection = () => (
-  <div className="w-100 py-5 bg-light">
-    <Row className="w-100 justify-content-center align-items-center">
-      <Col>
-        <Row>
-          <Col className="text-center pb-4">
-            <h4><strong>What is RepCompanion?</strong></h4>
-            <h6>UH RepCompanion is an application designed to connect University of Hawai&apos;i students seeking gym partners.</h6>
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col>
-            <Image className="mb-3" style={{ width: '500px', height: 'auto' }} src="./images/uh-repcompanion-high-five.jpeg" />
-          </Col>
-          <Col>
-            <Image className="mb-3" style={{ width: '500px', height: 'auto' }} src="./images/uh-repcompanion-using-app.jpeg" />
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col className="m-3">
-            <p>By matching users based on fitness goals, interests, availability,
-              and preferred workout styles, RepCompanion helps students overcome
-              shyness and build confidence while providing motivation and accountability
-              to stick to their fitness routines.
-            </p>
-          </Col>
-          <Col className="m-3">
-            <p>With RepCompanion, students can find compatible workout buddies who can push them to achieve their fitness goals, try new exercises, and stay committed to their health and wellness journey.</p>
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col>
-            <Image style={{ width: '500px', height: 'auto' }} src="./images/uh-repcompanion-connection.jpg" />
-          </Col>
-          <Col>
-            <Image style={{ width: '500px', height: 'auto' }} src="./images/uh-repcompanion-ritual.jpg" />
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </div>
-);
+const SecondSection = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    // Set visible to true when the component mounts
+    setVisible(true);
+  }, []);
+
+  return (
+    <div className={`w-100 py-4 white-footer-gradient infofooter fade-in ${visible ? 'visible' : ''}`} style={{ color: 'black' }}>
+      <Row className="w-100 justify-content-center align-items-center">
+        <Col>
+          <Row>
+            <Col className="text-center pb-3">
+              <div className="bg-light pt-1" style={{ width: '500px', margin: '0 auto', border: '2px solid black' }}>
+                <h4 className="pb-2"><strong>What is RepCompanion?</strong></h4>
+                <h6>UH RepCompanion is an application designed to connect University of Hawai&apos;i students seeking gym partners.</h6>
+              </div>
+            </Col>
+          </Row>
+          <Row className="text-center">
+            <Col>
+              <Image className="mb-3" style={{ width: '500px', height: 'auto', border: '2px solid black' }} src="./images/uh-repcompanion-high-five.jpeg" />
+            </Col>
+            <Col>
+              <Image className="mb-3" style={{ width: '500px', height: 'auto', border: '2px solid black' }} src="./images/uh-repcompanion-using-app.jpeg" />
+            </Col>
+          </Row>
+          <Row className="text-center">
+            <Col className="m-2">
+              <div className="bg-light" style={{ width: '500px', marginLeft: '88px', padding: '5px', border: '2px solid black' }}>
+                <p style={{ width: '490px' }}>By matching users based on fitness goals, interests, availability,
+                  and preferred workout styles, RepCompanion helps students overcome
+                  shyness and build confidence while providing motivation and accountability
+                  to stick to their fitness routines.
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <Image width={150} height={150} style={{ borderRadius: '50%', border: '1px solid white' }} className="rounded-circle" src="../images/uh-repcompanion.png" />
+            </Col>
+            <Col className="m-2">
+              <div className="bg-light pt-1" style={{ width: '500px', marginRight: '88px', border: '2px solid black' }}>
+                <p style={{ width: '490px' }}>
+                  With RepCompanion, students can find and connect with compatible workout buddies who can push them to achieve their fitness goals, try new exercises, and help them stay committed to their health and wellness journey.
+                </p>
+              </div>
+            </Col>
+          </Row>
+          <Row className="text-center mt-0">
+            <Col>
+              <Image style={{ width: '500px', height: 'auto', border: '2px solid black' }} src="./images/uh-repcompanion-connection.jpg" />
+            </Col>
+            <Col>
+              <Image style={{ width: '500px', height: 'auto', border: '2px solid black' }} src="./images/uh-repcompanion-ritual.jpg" />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default SecondSection;
