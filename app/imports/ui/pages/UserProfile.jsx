fdf15d94cdbb5d0d3295891d35191c695bdf4c19
Brandon Nelson <bksnelson1@gmail.com>
2024-05-08 01:24:46 -1000

    Update UserProfile No event card.

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -11,6 +11,7 @@ import UserEventCard from '../components/UserEventCard';
 import { Schedules } from '../../api/schedule/Schedules';
 import { ProfilesSchedules } from '../../api/profiles/ProfilesSchedules';
 import { ProfilesEvents } from '../../api/profiles/ProfilesEvents';
+import OtherUserEventCard from '../components/OtherUserEventCard';
 
 const UserProfile = () => {
   const { ready, profile, event } = useTracker(() => {
@@ -35,20 +36,8 @@ const UserProfile = () => {
       <Row>
         <UserProfileCard profile={profile} />
       </Row>
-      <Row>{event ? <UserEventCard profile={profile} event={event} /> : (
-        <Container className="d-flex justify-content-center align-items-center infofooter" style={pageStyle}>
-          <Row className="justify-content-center align-items-center">
-            <Col>
-              <Col className="justify-content-center text-center"><h2 style={{ color: 'white' }}>{profile?.firstName}&apos;s Event</h2></Col>
-              <Card style={{ width: '600px', height: '600px', backgroundColor: 'white', border: '1px solid black' }}>
-                <Card.Body className="d-flex flex-column justify-content-between" style={{ width: '800', height: 'auto' }}>
-                  <h2>No Event</h2>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      )}
+      <Row>
+        {event ? <UserEventCard profile={profile} event={event} /> : <h2 style={{ color: 'white' }}>No Event</h2>}
       </Row>
     </Container>
   ) : <LoadingSpinner />;
