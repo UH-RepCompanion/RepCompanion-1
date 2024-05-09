fc5c4fd5f44811424c525038db446259e846154f
Brandon Nelson <bksnelson1@gmail.com>
2024-05-08 22:20:59 -1000

    1

--- app/imports/ui/components/FirstSection.jsx
+++ app/imports/ui/components/FirstSection.jsx
@@ -24,20 +24,18 @@ const FirstSection = () => {
           <h1 className="welcome" style={{ color: 'white' }}><strong>Welcome to</strong></h1>
           <h1 className="welcome" style={{ color: 'white' }}><strong>UH RepCompanion!</strong></h1>
           {currentUser ? (
-            <Row>
-              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-              <a href="/events">
-                <Calendar2Event style={{ color: 'lightcyan' }} size={175} />
-              </a>
-            </Row>
+            // eslint-disable-next-line jsx-a11y/control-has-associated-label
+            <a href="/events">
+              <Calendar2Event style={{ color: 'lightcyan' }} size={150} />
+            </a>
           ) : (
             <Button className="py-3 justify-content-center" variant="dark" id="landing-page-button" style={{ width: '150px' }} as={Link} to="/signin"><strong> Login </strong></Button>
           )}
-          <Image width={250} height={250} style={{ marginTop: '20px', marginBottom: '20px', marginRight: '20px', marginLeft: '20px', borderRadius: '50%', border: '3px solid white' }} className="rounded-circle" src="../images/uh-repcompanion.png" />
+          <Image width={300} height={300} style={{ marginTop: '20px', marginBottom: '20px', marginRight: '20px', marginLeft: '20px', borderRadius: '50%', border: '3px solid white' }} className="rounded-circle" src="../images/uh-repcompanion.png" />
           {currentUser ? (
             // eslint-disable-next-line jsx-a11y/control-has-associated-label
             <a href="/filter">
-              <PersonCircle style={{ color: 'lightcyan' }} size={175} />
+              <PersonCircle style={{ color: 'lightcyan' }} size={150} />
             </a>
           ) : (
             <Button className="py-3 justify-content-center" variant="dark" id="landing-page-button" style={{ width: '150px' }} as={Link} to="/signup"><strong> Register </strong></Button>
